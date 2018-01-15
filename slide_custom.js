47c9cda2083e44813f95a1335a72e138f31921e7
jessicaQll <31117376+jessicaQll@users.noreply.github.com>
2018-01-15 19:23:58 +0800

    slide1.1
    在上一版本的基础上完善功能，此插件可根据需轮播的子元素数量、每次移动子元素的个数及可见区显示的总个数进行设置，实现不同需求的轮播

--- slide_custom.js
+++ slide_custom.js
@@ -3,51 +3,90 @@
 
         var defaults = {
             moveStep: 1,//元素移动个数
-            showIndex: 5, //显示区显示的个数
-            imgQuantity : 10,//轮播实际个数
-            slideStep : 240,//轮播左移步长
+            showIndex: null, //显示区显示的个数
+            imgQuantity : null,//轮播实际个数
+            slideStep : null,//轮播左移步长
             slideInterval : 2000,//轮播时间间隔
             slideDuration : 1000,//动画滑动时间
-            prevClassName: "",//左侧移动按钮类名
-            nextClassName: "",//右侧移动按钮类名
-            slideEleClassName: "",//轮播元素类名
-            slideParentClassName: ""//轮播父元素类名
+            prevClassName: "prev-arrow",//左侧移动按钮类名
+            nextClassName: "next-arrow",//右侧移动按钮类名
+            slideEleClassName: "collect-box",//轮播元素类名
+            slideParentClassName: "box-parent",//轮播父元素类名
+            hasArrow: false//是否有箭头点击效果
         };
 
         var opts = $.extend(defaults, options || {});
-
         var imgIndex = opts.showIndex;//显示区第一个元素,即显示区显示的个数
         var currentLeft;//当前left值
         var slideTimer = null;//轮播定时器
-        var boxWidth = opts.slideStep * (opts.imgQuantity + opts.showIndex *2);//轮播父元素宽度
         var $slideEle = $("." + opts.slideEleClassName);//轮播元素
         var $slideEleParent = $("." + opts.slideParentClassName);//轮播父元素
         var $prevClassName = $("." + opts.prevClassName);//左侧移动按钮
         var $nextClassName = $("." + opts.nextClassName);//右侧移动按钮
+        // var maxIndex = null;//轮播索引临界值
+
+        //计算显示 显示个数与轮播实际个数的最小公倍数
+        var virtualQuantity = lowestCommonMultiple(opts.showIndex,opts.imgQuantity,opts.moveStep);
 
         var $slideLists = $slideEle.children();
-        //获取轮播item的前showIndex个元素
-        var $frontEle = $slideLists.slice(0, opts.showIndex);
-        //获取轮播item的后showIndex个元素
-        var $backEle = $slideLists.slice(opts.imgQuantity - opts.showIndex, opts.imgQuantity);
 
-        //动态添加轮播前后附属图
-        $frontEle.clone().appendTo($slideEle);
-        $backEle.clone().prependTo($slideEle);
+        //动态添加轮播子元素
+        for(var cycleIndex = 1; cycleIndex<virtualQuantity/opts.imgQuantity; cycleIndex++){
+            $slideLists.clone(true).appendTo($slideEle);
+        }
+        //获取轮播item的前showIndex个元素
+        var $frontEle = $slideLists.slice(0,opts.showIndex);
+        $frontEle.clone(true).appendTo($slideEle);//继续添加轮播后附属图
+
+        //存在箭头点击事件，则继续补充轮播子元素
+        if(opts.hasArrow) {
+            if(opts.moveStep != 1){
+                for (var cycleIndex = 0; cycleIndex < virtualQuantity / opts.imgQuantity; cycleIndex++) {
+                    $slideLists.clone(true).prependTo($slideEle);
+                }
+            }
+            //获取轮播item的后showIndex个元素
+            var $backEle = $slideLists.slice(opts.imgQuantity - opts.showIndex, opts.imgQuantity);
+            $backEle.clone(true).prependTo($slideEle);//继续添加轮播前附属图
+        }
+
+        if(opts.hasArrow){
+            imgIndex = virtualQuantity + opts.showIndex;
+            // var maxIndex = $slideEle.children().length - opts.showIndex;//轮播索引临界值
+        }else{
+            // var maxIndex = $slideEle.children().length;//轮播索引临界值
+            imgIndex = 0;
+        }
+        var boxWidth = opts.slideStep * $slideEle.children().length;//轮播父元素宽度
+        var maxIndex = $slideEle.children().length - opts.showIndex;//轮播索引临界值
+        $slideEle.css("left" , -imgIndex * opts.slideStep);//设置初始最左端显示item
 
         //设置.box-parent宽度，是轮播item在一排显示
         $slideEleParent.width(boxWidth);//宽度=imgIndex*2 + slideStep * 元素个数
-        //设置初始最左端显示item
-        $slideEle.css("left" ,-imgIndex * opts.slideStep );
+
+        console.log($slideEle.children().length);
+
+        //计算三个数的最小公倍数
+        function lowestCommonMultiple(a,b,c){
+            for(var maxVal = Math.max(a,b); maxVal % a || maxVal % b; maxVal++){}
+            var arguments = maxVal;
+            for( maxVal = Math.max(arguments,c); maxVal % arguments || maxVal % c; maxVal++){}
+            return maxVal;
+        }
         //轮播
         function slide(){
             slideTimer = setInterval(function() {
                 currentLeft = parseInt($slideEle.css("left"));
+                // console.log(imgIndex);
                 imgIndex = imgIndex + opts.moveStep;
                 $slideEle.animate({ "left": currentLeft - opts.slideStep * opts.moveStep }, opts.slideDuration, function(){
-                    if(imgIndex >= opts.imgQuantity + opts.showIndex){
-                        imgIndex = opts.showIndex;
-                        $slideEle.css( "left" , -imgIndex * opts.slideStep    );
+                    if(imgIndex >= maxIndex){
+                        if(opts.hasArrow){
+                            imgIndex = virtualQuantity + opts.showIndex;
+                        }else{
+                            imgIndex = 0;
+                        }
+                        $slideEle.css( "left" , -imgIndex * opts.slideStep );
                     }
                 });
             },opts.slideInterval);
@@ -62,10 +101,9 @@
                 currentLeft = parseInt($slideEle.css("left"));
                 imgIndex = imgIndex - opts.moveStep;
                 $slideEle.animate({ "left" : currentLeft + opts.slideStep * opts.moveStep }, opts.slideDuration, function(){
-                    if(imgIndex <= 0){
-                        imgIndex = opts.imgQuantity;
-                        console.log(imgIndex);
-                        $slideEle.css( "left" , -imgIndex * opts.slideStep );
+                    if(imgIndex <= opts.showIndex){
+                        imgIndex = virtualQuantity + opts.showIndex;
+                        $slideEle.css("left", -imgIndex * opts.slideStep);
                     }
                 } );
             });
@@ -77,8 +115,8 @@
                 currentLeft = parseInt($slideEle.css("left"));
                 imgIndex = imgIndex + opts.moveStep;
                 $slideEle.animate({ "left" : currentLeft - opts.slideStep * opts.moveStep }, opts.slideDuration, function(){
-                    if(imgIndex >= opts.imgQuantity + opts.showIndex){
-                        imgIndex = opts.showIndex;
+                    if(imgIndex >= maxIndex){
+                        imgIndex = virtualQuantity + opts.showIndex;
                         $slideEle.css( "left" , -imgIndex * opts.slideStep );
                     }
                 } );
@@ -96,7 +134,9 @@
             $prevClassName.hide();
             $nextClassName.hide();
         });
-
+        if(!opts.hasArrow){
+            $prevClassName.off();
+        }
         slide();
         arrowStep();
     }
